3f5b04b8b8d7d340c217030f4454587189d8c510
TomposA <annaxilef@gmail.com>
2021-09-13 21:25:13 +0200

    First commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,53 @@
 
 @section('content')
 
-<p>Szia!</p>
+<div class="row">
+<div class="col s6 m12">
+      <div class="card #e65100 orange darken-4">
+        <div class="card-content white-text">
+          <span class="card-title">Napi viccek</span>
+          <p>Itt van pár vicc, hogy vidámabban induljon a napod!</p>
+        </div>
+        <div class="card-action">
+          <a href="#">Ez a link nem jó semmire.</a>
+          <a href="#">Ez se jó semmire.</a>
+        </div>
+      </div>
+    </div>
+<div
+<div class="row">
+    <div class="col s8 m6">
+    <div class="card #ffcc80 orange lighten-3">
+        <div class="card-content black-text">
+          <span class="card-title">Vicc 1</span>
+          <p>Mit kap a kisvidra, ha kijön a vízből? ... Levegőt.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s8 m6">
+      <div class="card #ffb74d orange lighten-2">
+        <div class="card-content black-text">
+          <span class="card-title">Vicc 2</span>
+          <p>Mi az, négy lába van, mégis eldől? ... Hosszában félbevágott pók.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s8 m6">
+      <div class="card #ffb74d orange lighten-2">
+        <div class="card-content black-text">
+          <span class="card-title">Vicc 3</span>
+          <p>Mit csinál a zsiráf, ha szembeköpöd? ... Kirúgja alólad a létrát.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s8 m6">
+    <div class="card #ffcc80 orange lighten-3">
+        <div class="card-content black-text">
+          <span class="card-title">Vicc 4</span>
+          <p>Miről lehet felismerni a repülő nyulat? ... Sas van a hátán.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 
 @endsection
