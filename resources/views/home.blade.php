47944c57a288d42ed0ac40aa69a4a2796be91064
TomposA <90270971+TomposA@users.noreply.github.com>
2021-09-27 21:34:50 +0200

    Created lo (#9)
    * First commit
    
    * jokes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,50 @@
 
 @section('content')
 
-<p>Hali!!!</p>
+<div class="row">
+<div class="col s6 m12">
+      <div class="card #e65100 orange darken-4">
+        <div class="card-content white-text">
+          <span class="card-title">Napi viccek</span>
+          <p>Itt van pár vicc, hogy vidámabban induljon a napod!</p>
+        </div>
+      </div>
+    </div>
+<div
+<div class="row">
+    <div class="col s8 m6">
+    <div class="card #ffcc80 orange lighten-3">
+        <div class="card-content black-text">
+          <span class="card-title">Vicc 1</span>
+          <p>Mit kap a kisvidra, ha kijön a vízből? ... Levegőt.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s8 m6">
+      <div class="card #ffb74d orange lighten-2">
+        <div class="card-content black-text">
+          <span class="card-title">Vicc 2</span>
+          <p>Mi az, négy lába van, mégis eldől? ... Hosszában félbevágott pók.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s8 m6">
+      <div class="card #ffb74d orange lighten-2">
+        <div class="card-content black-text">
+          <span class="card-title">Vicc 3</span>
+          <p>Mit csinál a zsiráf, ha szembeköpöd? ... Kirúgja alólad a létrát.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s8 m6">
+    <div class="card #ffcc80 orange lighten-3">
+        <div class="card-content black-text">
+          <span class="card-title">Vicc 4</span>
+          <p>Miről lehet felismerni a repülő nyulat? ... Sas van a hátán.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 
 <div class="row">
     <div class="col s2">
